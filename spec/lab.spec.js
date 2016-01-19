ba3557e43b6959a24401f086c140b7d89575f6fd
Antony Donovan <antony.donovan@generalassemb.ly>
2016-01-19 12:58:12 -0500

    Clarify that functions require arguments.
    Close #3
    
    Remove `paragraph` from `lib/lab.js` to avoid confusion.

--- spec/lab.spec.js
+++ spec/lab.spec.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var paragraph =
-  'When in the Course of human events, it becomes necessary for one ' +
-  'people to dissolve the political bands which have connected them ' +
-  'with another, and to assume among the powers of the earth, the ' +
-  'separate and equal station to which the Laws of Nature and of ' +
-  'Nature\'s God entitle them, a decent respect to the opinions of ' +
-  'mankind requires that they should declare the causes which impel ' +
-  'them to the separation.';
+  'We the People of the United States, in Order to form a more ' +
+  'perfect Union, establish Justice, insure domestic Tranquility, ' +
+  'provide for the common defence, promote the general Welfare, and ' +
+  'secure the Blessings of Liberty to ourselves and our Posterity, ' +
+  'do ordain and establish this Constitution for the United States ' +
+  'of America.';
 
 var collFuncs = require('../lib/lab.js');
 
