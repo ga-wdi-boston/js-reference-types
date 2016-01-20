473688aaeb4737818a54c286229e46e3547fbfca
Antony Donovan <antony.donovan@generalassemb.ly>
2016-01-19 20:30:47 -0500

    Update tests
    Redo tests for update clarifying `lib/lab.js` functions
    require arguments.

--- spec/lab.spec.js
+++ spec/lab.spec.js
@@ -15,20 +15,19 @@ describe('collections lab', function() {
   describe('normalized words', function() {
 
     const normalizedWords = [
-      'a', 'among', 'and', 'and', 'and', 'another', 'assume', 'bands',
-      'becomes', 'causes', 'connected', 'course', 'decent', 'declare',
-      'dissolve', 'earth', 'entitle', 'equal', 'events', 'for', 'god',
-      'have', 'human', 'impel', 'in', 'it', 'laws', 'mankind', 'nature',
-      'nature\'s', 'necessary', 'of', 'of', 'of', 'of', 'of', 'one',
-      'opinions', 'people', 'political', 'powers', 'requires',
-      'respect', 'separate', 'separation', 'should', 'station', 'that',
-      'the', 'the', 'the', 'the', 'the', 'the', 'the', 'the', 'the',
-      'them', 'them', 'them', 'they', 'to', 'to', 'to', 'to', 'to',
-      'when', 'which', 'which', 'which', 'with',
+      'a', 'america', 'and', 'and', 'and', 'blessings', 'common',
+      'constitution', 'defence', 'do', 'domestic', 'establish',
+      'establish', 'for', 'for', 'form', 'general', 'in',
+      'insure', 'justice', 'liberty', 'more', 'of', 'of', 'of',
+      'ordain', 'order', 'our', 'ourselves', 'people', 'perfect',
+      'posterity', 'promote', 'provide', 'secure', 'states',
+      'states', 'the', 'the', 'the', 'the', 'the', 'the', 'this',
+      'to', 'to', 'tranquility', 'union', 'united', 'united',
+      'we', 'welfare',
     ];
 
     it('has the correct length', function() {
-      expect(collFuncs.getNormalizedWords(paragraph).length).toBe(71);
+      expect(collFuncs.getNormalizedWords(paragraph).length).toBe(52);
     });
 
     it('matches expected array', function() {
@@ -41,18 +40,17 @@ describe('collections lab', function() {
   describe('unique words', function() {
 
     const sortedUniqueWords = [
-      'a', 'among', 'and', 'another', 'assume', 'bands', 'becomes',
-      'causes', 'connected', 'course', 'decent', 'declare', 'dissolve',
-      'earth', 'entitle', 'equal', 'events', 'for', 'god', 'have',
-      'human', 'impel', 'in', 'it', 'laws', 'mankind', 'nature',
-      'nature\'s', 'necessary', 'of', 'one', 'opinions', 'people',
-      'political', 'powers', 'requires', 'respect', 'separate',
-      'separation', 'should', 'station', 'that', 'the', 'them', 'they',
-      'to', 'when', 'which', 'with',
+      'a', 'america', 'and', 'blessings', 'common',
+      'constitution', 'defence', 'do', 'domestic', 'establish',
+      'for', 'form', 'general', 'in', 'insure', 'justice',
+      'liberty', 'more', 'of', 'ordain', 'order', 'our',
+      'ourselves', 'people', 'perfect', 'posterity', 'promote',
+      'provide', 'secure', 'states', 'the', 'this', 'to',
+      'tranquility', 'union', 'united', 'we', 'welfare',
     ];
 
     it('has the correct length', function() {
-      expect(collFuncs.getUniqueWords(paragraph).length).toBe(49);
+      expect(collFuncs.getUniqueWords(paragraph).length).toBe(38);
     });
 
     it('matches expected array', function() {
@@ -65,11 +63,11 @@ describe('collections lab', function() {
   describe('word count', function() {
 
     it('has the correct word count', function() {
-      expect(collFuncs.wordCount(paragraph)).toBe(71);
+      expect(collFuncs.wordCount(paragraph)).toBe(52);
     });
 
     it('has the correct unique word count', function() {
-      expect(collFuncs.wordCount(paragraph, true)).toBe(49);
+      expect(collFuncs.wordCount(paragraph, true)).toBe(38);
     });
 
   });
